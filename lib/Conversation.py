a4846a9ab48310b290cd6111877c8cb95240878b
Edmond Dooley <etdooley@ucsc.edu>
2016-12-04 14:44:56 -0800

    Added basic conversation class to start UI with.

---
+++ lib/Conversation.py
@@ -0,0 +1,22 @@
+class Conversation:
+    def __init__(self, data):
+        """
+            Takes the conversation data structure created using json.load()
+        """
+        pass
+    
+    def getAvailableActions(self):
+        """
+            Returns a list of strings with the names of available actions.
+        """
+        pass
+    
+    def submitAction(self, action):
+        """
+            Takes an action as a string and executes it if valid
+            If valid, it returns the chosen response, otherwise returns an empty string
+        """
+        pass
+    
+    def getDebugInfo(self):
+        pass
